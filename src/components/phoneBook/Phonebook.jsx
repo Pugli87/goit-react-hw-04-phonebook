06a6658733715573ad8c69b704c460e505f92fa8
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-08 22:22:48 -0500

    Update Phonebook.jsx

--- src/components/phoneBook/Phonebook.jsx
+++ src/components/phoneBook/Phonebook.jsx
@@ -1,84 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { Container, Title, Heading2 } from './stylesComponents/PhonebookStyled';
 import PropTypes from 'prop-types';
 
-class Phonebook extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+const Phonebook = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const updateLocalStorage = contacts => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   };
 
-  static propTypes = {
-    addContact: PropTypes.func,
-    deleteContact: PropTypes.func,
-  };
+  const addContact = newContact => {
+    setContacts(prevContacts => {
+      const newContacts = [...prevContacts, newContact];
 
-  /* Método que se ejecuta después de que el componente ha sido montado */
-  componentDidMount() {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      this.setState({ contacts: JSON.parse(storedContacts) });
-    }
-  }
-
-  /* Método que se ejecuta cada vez que el componente se actualiza */
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = newContact => {
-    this.setState({
-      contacts: [...this.state.contacts, newContact],
+      updateLocalStorage(newContacts);
+      return [...prevContacts, newContact];
     });
   };
 
-  deleteContact = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
+  const deleteContact = id => {
+    setContacts(prevContacts => {
+      const delContact = prevContacts.filter(contact => contact.id !== id);
+      updateLocalStorage(delContact);
+      return delContact;
     });
   };
 
-  setFilter = filterValue => {
-    this.setState({
-      filter: filterValue,
-    });
+  const setFilterValue = filterValue => {
+    setFilter(filterValue);
   };
 
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const filteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+  return (
+    <>
+      <Title>phonebook</Title>
+      <Container>
+        <ContactForm addContact={addContact} contacts={contacts} />
 
-    return (
-      <>
-        <Title>phonebook</Title>
-        <Container>
-          <ContactForm addContact={this.addContact} contacts={contacts} />
-
-          {contacts.length > 0 && (
-            <>
-              <Heading2>find contacts by name</Heading2>
-              <Filter filter={filter} setFilter={this.setFilter} />
-            </>
-          )}
-          <ContactList
-            contacts={this.filteredContacts()}
-            deleteContact={this.deleteContact}
-          />
-        </Container>
-      </>
-    );
-  }
-}
+        {contacts.length > 0 && (
+          <>
+            <Heading2>find contacts by name</Heading2>
+            <Filter filter={filter} setFilter={setFilterValue} />
+          </>
+        )}
+        <ContactList
+          contacts={filteredContacts()}
+          deleteContact={deleteContact}
+        />
+      </Container>
+    </>
+  );
+};
 
 export default Phonebook;
+
+Phonebook.propTypes = {
+  addContact: PropTypes.func,
+  deleteContact: PropTypes.func,
+};
